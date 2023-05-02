4a6c9cdc6fffc30ca08eea723ab413ed4d0c488c
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-02 13:51:22 +0200

    (feat): Add start page

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
 import { Nav } from "../components/Nav/Nav";
 
+import axios from "axios";
+import { IAnimal } from "../models/IAnimal";
+
 export const Start = () => {
+  const [animals, setAnimals] = useState<IAnimal[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      console.log("getData has been run");
+
+      const response = await axios.get<IAnimal[]>(
+        "https://animals.azurewebsites.net/api/animals"
+      );
+      setAnimals(response.data);
+    };
+    getData();
+  }, []);
+
   return (
     <>
       <Nav></Nav>
