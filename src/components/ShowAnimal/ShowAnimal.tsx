6561aad2118a4b2a28d8ef65febdc462c370da74
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-02 13:49:59 +0200

    (feat): Add ShowAnimal component

---
+++ src/components/ShowAnimal/ShowAnimal.tsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router";
+import { Nav } from "../Nav/Nav";
+
+export const ShowAnimal = () => {
+  const params = useParams();
+  return (
+    <>
+      <h1>ShowAnimal</h1>
+    </>
+  );
+};
