c59ce02f773a03d41185334e9f127580fc4c8fbb
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-02 13:51:05 +0200

    (feat): Add Animals page

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,9 +1,12 @@
 import { Nav } from "../components/Nav/Nav";
+import { ShowAnimal } from "../components/ShowAnimal/ShowAnimal";
+import { ShowAnimals } from "../components/ShowAnimals/ShowAnimals";
 
 export const Animals = () => {
   return (
     <>
       <Nav></Nav>
+      <ShowAnimals></ShowAnimals>
       <h1>Animals h1</h1>
     </>
   );
