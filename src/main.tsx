828b96e216651b92a724dc4c3747dc70ccd73184
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-02 11:01:46 +0200

    (build): Add SASS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import { Start } from "./pages/Start";
+import { Animals } from "./pages/Animals";
+// import { Error } from "./pages/Error";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Start></Start>,
+    // errorElement: <Error></Error>
+  },
+  {
+    path: "/animals",
+    element: <Animals></Animals>,
+  },
+]);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
