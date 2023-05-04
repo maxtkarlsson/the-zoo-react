e5509734ba449c62c4e03c817294ea634db74ff7
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-04 10:37:22 +0200

    (refactor): Change IAnimal to class

--- src/models/IAnimal.ts
+++ src/models/IAnimal.ts
@@ -1,4 +1,4 @@
-export interface IAnimal {
+/*export interface IAnimal {
   id: number;
   name: string;
   latinName: string;
@@ -9,4 +9,19 @@ export interface IAnimal {
   medicine: string;
   isFed: boolean;
   lastFed: string;
+}*/
+
+export class IAnimal {
+  constructor(
+    public id: number,
+    public name: string,
+    public latinName: string,
+    public yearOfBirth: number,
+    public shortDescription: string,
+    public longDescription: string,
+    public imageUrl: string,
+    public medicine: string,
+    public isFed: boolean,
+    public lastFed: string
+  ) {}
 }
