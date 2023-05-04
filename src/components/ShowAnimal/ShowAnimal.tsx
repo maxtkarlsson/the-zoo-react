05da2852c89f70030d7c37e87537f82a2dcfc65d
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-04 11:23:34 +0200

    (feat): Add property name to html in ShowAnimal

--- src/components/ShowAnimal/ShowAnimal.tsx
+++ src/components/ShowAnimal/ShowAnimal.tsx
@@ -1,9 +1,13 @@
-import { useParams } from "react-router";
 import { IAnimal } from "../../models/IAnimal";
+import { getLocalStorage } from "../../helpers/getLocalStorage";
 
 interface AnimalProps extends IAnimal {}
 
-// State som hämtar från localstorage när det uppdateras.
+const handleClick = (currentAnimalId: number) => {
+  const animalsFromLs = getLocalStorage();
+  //Uppdatera listan, map loop
+  //
+};
 
 export const ShowAnimal = ({
   id,
@@ -20,16 +24,16 @@ export const ShowAnimal = ({
   //const params = useParams();
   return (
     <div>
-      <p>{id}</p>
-      <h3>{name}</h3>
-      <p>{latinName}</p>
-      <p>{yearOfBirth}</p>
-      <p>{shortDescription}</p>
-      <p>{longDescription}</p>
-      <p>{imageUrl}</p>
-      <p>{medicine}</p>
-      <p>{isFed}</p>
-      <p>{lastFed}</p>
+      <p>id - {id}</p>
+      <h3>name - {name}</h3>
+      <p>latinName - {latinName}</p>
+      <p>yearOfBirth - {yearOfBirth}</p>
+      <p>shortDescription - {shortDescription}</p>
+      <p>longDescription - {longDescription}</p>
+      <p>imageUrl - {imageUrl}</p>
+      <p>medicine - {medicine}</p>
+      <p>isFed - {isFed}</p>
+      <p>lastFed - {lastFed}</p>
     </div>
   );
 };
