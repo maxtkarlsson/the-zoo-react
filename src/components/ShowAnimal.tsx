7386695012cc5e25c04a5d8ac601297f68db9e0f
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-04 17:49:39 +0200

    (feat): Add isFed boolean to html

--- src/components/ShowAnimal.tsx
+++ src/components/ShowAnimal.tsx
@@ -1,6 +1,8 @@
 import { IAnimal } from "../models/IAnimal";
 
 export const ShowAnimal = (animal: IAnimal) => {
+  const show = animal.isFed;
+
   return (
     <div>
       <p>id - {animal.id}</p>
@@ -11,7 +13,8 @@ export const ShowAnimal = (animal: IAnimal) => {
       <p>longDescription - {animal.longDescription}</p>
       <p>imageUrl - {animal.imageUrl}</p>
       <p>medicine - {animal.medicine}</p>
-      <p>isFed - {animal.isFed}</p>
+      {!show && <p>isFed - false</p>}
+      {show && <p>isFed - true</p>}
       <p>lastFed - {animal.lastFed}</p>
     </div>
   );
