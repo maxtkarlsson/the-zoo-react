0114835fa6ac91eb1181d8f2abbbda2585127e64
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-08 14:10:44 +0200

    (feat): Add classnames to ShowAnimal

--- src/components/ShowAnimal.tsx
+++ src/components/ShowAnimal.tsx
@@ -1,4 +1,5 @@
 import { IAnimal } from "../models/IAnimal";
+import "./ShowAnimal.scss";
 
 interface IShowAnimalProps extends IAnimal {
   longDesc: boolean;
@@ -22,29 +23,47 @@ export const ShowAnimal = (props: IShowAnimalProps) => {
 
   if (longDesc) {
     return (
-      <div>
-        <p>id - {id}</p>
-        <h3>{name}</h3>
-        <p>imageUrl - {imageUrl}</p>
-        <p>Född: {yearOfBirth}</p>
-        <p>{shortDescription}</p>
-        <p>Latinsk namn: {latinName}</p>
-        <p>{longDescription}</p>
-        <p>Medicin: {medicine}</p>
-        {!show && <p>{name} är hungrig.</p>}
-        {show && <p>{name} är mätt.</p>}
-        <p>Senast matad: {lastFed}</p>
+      <div className="animal-container">
+        <div className="animal-info">
+          <div className="animal-picture">
+            <img className="animal-picture__img" src={imageUrl}></img>
+          </div>
+          <div className="animal-desc">
+            <h3 className="animal-desc__name">{name}</h3>
+            <span className="animal-desc__text">{shortDescription}</span>
+            <p className="animal-desc__name--fact">Latinsk namn: {latinName}</p>
+            <span className="animal-desc__text--fact">{longDescription}</span>
+          </div>
+        </div>
+        <div className="animal-status">
+          <p className="animal-status__info">Född: {yearOfBirth}</p>
+          <p className="animal-status__info">Medicin: {medicine}</p>
+          {!show && (
+            <p className="animal-status__hunger--alert">{name} är hungrig.</p>
+          )}
+          {show && <p className="animal-status__hunger">{name} är mätt.</p>}
+          <p className="animal-status__date">Senast matad: {lastFed}</p>
+        </div>
       </div>
     );
   } else {
     return (
-      <div>
-        <p>id - {id}</p>
-        <h3>{name}</h3>
-        <p>imageUrl - {imageUrl}</p>
-        <p>{shortDescription}</p>
-        {!show && <p>{name} är hungrig.</p>}
-        {show && <p>{name} är mätt.</p>}
+      <div className="animal-container-s">
+        <div className="animal-info-s">
+          <div className="animal-picture-s">
+            <img className="animal-picture-s__img" src={imageUrl}></img>
+          </div>
+          <div className="animal-desc-s">
+            <h3 className="animal-desc-s__name">{name}</h3>
+            <span className="animal-desc-s__text">{shortDescription}</span>
+          </div>
+        </div>
+        <div className="animal-status-s">
+          {!show && (
+            <p className="animal-status-s__hunger--alert">{name} är hungrig.</p>
+          )}
+          {show && <p className="animal-status-s__hunger">{name} är mätt.</p>}
+        </div>
       </div>
     );
   }
